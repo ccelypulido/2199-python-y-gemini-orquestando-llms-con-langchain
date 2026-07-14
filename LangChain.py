64756c8ed24b78c8551c1b58cc383fa0b1d5fde4
Cesar Cely <ccelypulido@gmail.com>
2026-07-14 13:44:37 -0500

    Cambio en el LLM y ajuste en la versión instalada de cohere 5.15.0

--- LangChain.py
+++ LangChain.py
@@ -1,7 +1,8 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_cohere import ChatCohere
 from langchain_core.messages import HumanMessage
 from my_models import GEMINI_FLASH
-from my_keys import GEMINI_API_KEY
+from my_keys import GEMINI_API_KEY, COHERE_API_KEY
 from my_helper import encode_image
 
 llm = ChatGoogleGenerativeAI(
@@ -9,22 +10,30 @@ llm = ChatGoogleGenerativeAI(
     model=GEMINI_FLASH
 )
 
-imagen = encode_image("datos/ejemplo_grafico.jpg")
+respuesta = llm.invoke("Cuáles canales colombianos de youtube me recomiendas para saber más sobre teléfonos inteligentes?")
+print(f"Gemini: ",respuesta.content)
 
-pregunta = "Describe la imagen:"
-
-mensaje = HumanMessage(
-    content = [
-        {
-            "type": "text",
-            "text": pregunta
-        },
-        {
-            "type": "image_url",
-            "image_url": f"data:image/jpeg;base64,{imagen}"
-        }
-    ]
+llm = ChatCohere(
+    cohere_api_key=COHERE_API_KEY
 )
+respuesta = llm.invoke([HumanMessage(content="Cuáles canales colombianos de youtube me recomiendas para saber más sobre teléfonos inteligentes?")])
+print(f"Cohere: ",respuesta.content)
+# imagen = encode_image("datos/ejemplo_grafico.jpg")
 
-respuesta = llm.invoke([mensaje])
-print(respuesta)
+# pregunta = "Describe la imagen:"
+
+# mensaje = HumanMessage(
+#     content = [
+#         {
+#            "type": "text",
+#            "text": pregunta
+#        },
+#        {
+#            "type": "image_url",
+#            "image_url": f"data:image/jpeg;base64,{imagen}"
+#        }
+#    ]
+#)
+
+#respuesta = llm.invoke([mensaje])
+#print(respuesta)

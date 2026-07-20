8527e0815b4c8d1a4e2bc91afd7b4288ef1610c3
Cesar Cely <ccelypulido@gmail.com>
2026-07-20 11:02:50 -0500

    main

---
+++ main.py
@@ -0,0 +1,19 @@
+from langchain.agents import AgentExecutor
+from orquestador import AgenteOrquestador
+
+def main():
+    agente = AgenteOrquestador()
+    ejecutor = AgentExecutor(
+                    agent = agente.agente,
+                    tools = agente.tools,
+                    verbose = True
+                )
+    
+    pregunta = "Realiza el análisis de la imagen ejemplo_grafico.jpg"
+
+    respuesta = ejecutor.invoke({"input" : pregunta})
+
+    print(respuesta)
+
+if __name__=="__main__":
+    main()
